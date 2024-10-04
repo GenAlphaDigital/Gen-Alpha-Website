f8e690b49b64362412d25d8066c6cc6b32093c57
Akhileswar <akhileswarsathivada@gmail.com>
2024-10-04 19:35:44 +0530

    Contact Popup

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -14,10 +14,12 @@ const popup = {
   hidden: {
     opacity: 0,
     scale: 0,
+    x: -100,
   },
   visible: {
     opacity: 1,
     scale: 1,
+    x: 0,
     transition: {
       duration: 0.5,
     },
