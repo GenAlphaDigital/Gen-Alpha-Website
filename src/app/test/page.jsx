0415d2c9debd3cb5d61c9d866fc6c141080c9c1b
Akhileswar <akhileswarsathivada@gmail.com>
2024-10-13 19:04:45 +0530

    Test Page For SVG Animation

--- src/app/test/page.jsx
+++ src/app/test/page.jsx
@@ -1,24 +1,63 @@
 "use client";
-import Marketing from "@/components/3d Components/marketing";
-import Packaging from "@/components/3d Components/packaging";
-import Branding from "@/components/3d Components/branding";
-import WebDev from "@/components/3d Components/webdev";
-import Contact from "@/components/Contact/Contact";
+import { useEffect } from "react";
 import styles from "./page.module.css";
 
-const page = () => {
+const Page = () => {
+  useEffect(() => {
+    const svg = document.querySelector("svg.curve");
+    const path = document.querySelector("svg.curve path");
+    const pathLength = path.getTotalLength();
+
+    // Initially set the strokeDasharray and strokeDashoffset
+    path.style.strokeDasharray = pathLength;
+    path.style.strokeDashoffset = pathLength;
+
+    const handleScroll = () => {
+      // Get the scroll position of the page
+      const scrollValue = window.scrollY;
+
+      // Get the height of the document
+      const documentHeight =
+        document.documentElement.scrollHeight - window.innerHeight;
+
+      // Calculate scroll percentage
+      let scrollPercent = scrollValue / documentHeight;
+
+      // If we are in the last 25% of the path, reduce the scroll speed
+      if (scrollPercent > 0.75) {
+        const slowdownFactor = (scrollPercent - 0.75) * 2; // Increases as you approach 100%
+        scrollPercent = 0.75 + slowdownFactor / 2; // Adjust to slow down
+      }
+
+      // Calculate the current position of the stroke
+      const currentPoint = scrollPercent * pathLength * 1.1;
+
+      // Update the strokeDashoffset to animate the path, apply slow down factor
+      path.style.strokeDashoffset = pathLength - currentPoint;
+    };
+
+    // Add scroll event listener
+    window.addEventListener("scroll", handleScroll);
+
+    // Clean up event listener on component unmount
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
+
   return (
     <div className={styles.container}>
-      <div className={styles.lineContainer}>
+      <div className={styles.svgContainer}>
         <svg
-          width="699"
-          height="4195"
+          width="100%"
+          height="6000"
+          className="curve"
+          viewBox="0 0 713 5178"
           fill="none"
-          preserveAspectRatio="xMidYMax meet"
         >
           <path
-            d="M615.5 0.5V432.5C615.5 465.637 588.637 492.5 555.5 492.5H62.5C29.3629 492.5 2.5 519.363 2.5 552.5V970.5C2.5 1003.64 29.3629 1030.5 62.5 1030.5H555.5C588.637 1030.5 615.5 1057.36 615.5 1090.5V1313.5L539 1390L615.5 1466.5L692 1390H615.5V1996.5L539 2073L615.5 2149.5L692 2073H615.5V2772L539 2848.5L615.5 2925L692 2848.5H615.5V3598L539 3674.5L615.5 3751L692 3674.5H615.5V4061.5M615.5 4061.5L577.25 4099.75C556.125 4120.87 556.125 4155.13 577.25 4176.25V4176.25C598.375 4197.37 632.625 4197.37 653.75 4176.25V4176.25C674.875 4155.13 674.875 4120.87 653.75 4099.75L615.5 4061.5Z"
-            stroke="red"
+            d="M602.5 0V414C602.5 447.137 575.637 474 542.5 474H62.5C29.3629 474 2.5 500.863 2.5 534V947C2.5 980.137 29.3629 1007 62.5 1007H542.5C575.637 1007 602.5 1033.86 602.5 1067V1374L499 1477.5L602.5 1581L706 1477.5H602.5V2343L499 2446.5L602.5 2550L706 2446.5H602.5V3201.5L499 3305L602.5 3408.5L706 3305H602.5C602.5 3305 602.5 3762.41 602.5 4055.5L499 4159L602.5 4262.5L706 4159H602.5V4967M602.5 4967L499 5070.5L602.5 5174L706 5070.5L602.5 4967Z"
+            stroke="black"
             strokeWidth="5"
           />
         </svg>
@@ -27,4 +66,4 @@ const page = () => {
   );
 };
 
-export default page;
+export default Page;
