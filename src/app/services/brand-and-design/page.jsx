bb6abefcb0133e13589c285914f64e379b36b02b
Akhileswar <akhileswarsathivada@gmail.com>
2024-10-17 12:19:12 +0530

    Changes in the Services Category

--- src/app/services/brand-and-design/page.jsx
+++ src/app/services/brand-and-design/page.jsx
@@ -1,7 +1,28 @@
-import React from "react";
+import Image from "next/image";
+import styles from "./page.module.css";
+import DropDown from "../component/DropDown";
 
 const page = () => {
-  return <div></div>;
+  return (
+    <div className={styles.container}>
+      <div className={styles.logo}>
+        <Image src={"/Logo.svg"} alt="" width={120} height={120} />
+      </div>
+      <div className={styles.wrapper}>
+        <div className={styles.dropDown}>
+          <DropDown />
+        </div>
+        <div className={styles.content}>
+          <h1>Brand And Design Services</h1>
+          <p>
+            Your brand shouldn’t just be out there—it should own the spotlight,
+            turning heads and leaving a mark wherever it goes. Here’s how we
+            bring the spark that makes your brand unforgettable.
+          </p>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default page;
