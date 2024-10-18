d233fb0f6d242a304f5da26d54b2266c7035a712
YashRenwa <yash.renwa@gmail.com>
2024-10-18 17:34:44 +0530

    Refactor next.config.mjs and package.json
    - Update next.config.mjs to include output and trailingSlash properties.
    - Modify package.json to set the start script to use NODE_ENV=production.
    
    Remove unused files
    
    - Delete src/app/about/page.jsx and src/app/about/page.module.css.
    
    Add server.js
    
    - Create a new file server.js with server configuration.
    
    Update CSS styles
    
    - Remove unused CSS styles from src/app/about/page.module.css.

--- src/app/about/page.jsx
+++
@@ -1,6 +0,0 @@
-import dynamic from "next/dynamic";
-
-// Dynamically import the component with SSR disabled
-const AboutPage = dynamic(() => import("./About.jsx"), { ssr: false });
-
-export default AboutPage;
