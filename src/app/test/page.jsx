6cf928aafbb10efffca1b60d5a00fafc2c02d28a
Akhileswar1905 <akhileswarsathivada@gmail.com>
2024-10-25 18:52:40 +0530

    Changes in test page

--- src/app/test/page.jsx
+++ src/app/test/page.jsx
@@ -2,26 +2,28 @@ import React from "react";
 
 const page = () => {
   return (
-    <div>
-      <div class="sketchfab-embed-wrapper w-[80%] h-[80%]">
-        {" "}
-        <iframe
-          title="Wolf Animated"
-          frameborder="0"
-          // allowfullscreen
-          // mozallowfullscreen="true"
-          webkitallowfullscreen="true"
-          allow="autoplay; fullscreen; xr-spatial-tracking"
-          xr-spatial-tracking
-          execution-while-out-of-viewport
-          className="w-[100%] h-[100%]"
-          execution-while-not-rendered
-          web-share
-          src="https://sketchfab.com/models/5134c24977e04c76ac403acb1585f7e8/embed"
-        >
+    <div className="w-[80%] h-[80%]">
+      <div className=" overflow-hidden">
+        <div class="sketchfab-embed-wrapper w-full h-[150vh] absolute ">
           {" "}
-        </iframe>{" "}
-        {/* <p
+          <iframe
+            title="Wolf Animated"
+            frameborder="0"
+            // allowfullscreen
+            // mozallowfullscreen="true"
+            webkitallowfullscreen="true"
+            allow="autoplay; fullscreen; xr-spatial-tracking"
+            xr-spatial-tracking
+            execution-while-out-of-viewport
+            className="w-[100%] h-[100%]"
+            execution-while-not-rendered
+            autoPlay={true}
+            web-share
+            src="https://sketchfab.com/models/5134c24977e04c76ac403acb1585f7e8/embed"
+          >
+            {" "}
+          </iframe>{" "}
+          {/* <p
           style={{
             fontSize: "13px",
             fontWeight: "normal",
@@ -59,6 +61,7 @@ const page = () => {
             Sketchfab
           </a>
         </p> */}
+        </div>
       </div>
     </div>
   );
