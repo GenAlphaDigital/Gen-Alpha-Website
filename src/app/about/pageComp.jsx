92237065bed1efa1db24780a4ada22be57c27154
E <etikivishwas47@gmail.com>
2024-10-28 23:44:28 -0700

    about us svg changes

--- src/app/about/pageComp.jsx
+++ src/app/about/pageComp.jsx
@@ -52,7 +52,11 @@ const Page = () => {
       const totalDistance = 3645;
       const scrollPercent = scrollValue / totalDistance;
       const currentPoint = scrollPercent * pathLength;
-      path.style.strokeDashoffset = (pathLength - currentPoint) * 1.5;
+      if (scrollPercent < 0.8) {
+        path.style.strokeDashoffset = (pathLength - currentPoint) * 1.3;
+      } else {
+        path.style.strokeDashoffset = (pathLength - currentPoint) * 0.8;
+      }
     };
 
     const observer = new IntersectionObserver(
