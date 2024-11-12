4e5e8cdf8115416339b93fc67f89fab397c3e5c2
Akhileswar1905 <akhileswarsathivada@gmail.com>
2024-11-12 14:24:37 +0530

    About us page completed

--- src/app/about/page.jsx
+++ src/app/about/page.jsx
@@ -1,9 +1,62 @@
+"use client";
+
+import { useState, useEffect } from "react";
 import Image from "next/image";
 import MainFrame from "../../../public/about/svg";
+import Tablet from "../../../public/about/Tablet";
+import Phone from "../../../public/about/Phone";
 import styles from "./page.module.css";
-// import Page from "./pageComp";
 
 const AboutPage = () => {
+  const [screenWidth, setScreenWidth] = useState(null);
+
+  useEffect(() => {
+    // Set the initial screen width and update it on resize
+    const updateScreenWidth = () => setScreenWidth(window.innerWidth);
+    updateScreenWidth(); // Set initial width
+    window.addEventListener("resize", updateScreenWidth);
+
+    // Clean up the event listener on component unmount
+    return () => window.removeEventListener("resize", updateScreenWidth);
+  }, []);
+
+  // Conditional rendering based on screen width
+  if (screenWidth < 450) {
+    return (
+      <>
+        <div className={styles.header}>
+          <Image
+            src={"/Logo.svg"}
+            alt="logo"
+            width={120}
+            height={120}
+            priority={true}
+          />
+        </div>
+        <div className={`${styles.svgContainer}`}>
+          <Phone />
+        </div>
+      </>
+    );
+  } else if (screenWidth <= 768) {
+    return (
+      <>
+        <div className={styles.header}>
+          <Image
+            src={"/Logo.svg"}
+            alt="logo"
+            width={120}
+            height={120}
+            priority={true}
+          />
+        </div>
+        <div className={`${styles.svgContainer}`}>
+          <Tablet />
+        </div>
+      </>
+    );
+  }
+
   return (
     <>
       <div className={styles.header}>
@@ -15,10 +68,11 @@ const AboutPage = () => {
           priority={true}
         />
       </div>
-      <div className={styles.svgContainer}>
+      <div className={`${styles.svgContainer}`}>
         <MainFrame />
       </div>
     </>
   );
 };
+
 export default AboutPage;
